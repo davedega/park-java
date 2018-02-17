8dfca517db08f0dc973462d4d082fa070bfeece4
davedega <dasher_@hotmail.com>
2018-02-17 20:40:51 +0100

    Add project

---
+++ app/src/main/java/com/dega/parkjava/ParkActivity.java
@@ -0,0 +1,13 @@
+package com.dega.parkjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_park);
+    }
+}
